79595061f9ac7f1bbacecdf00381ddcf343e4fd3
FEPAG5 <fepag_05@yahoo.com>
2025-06-29 17:28:29 +0800

    Create A12-250701.ts

---
+++ src/data/LCN/BATCH-12/A12-250701.ts
@@ -0,0 +1,17 @@
+const lcnData = {
+  lcn: "A12-250701",
+  name: "Legal Name",
+  issued: "Issued Date",
+  expiration: "Expiry Date",
+  registration: "Latest Registration Date",
+  FWE: "00",
+  SJE: "00",
+  EP: "00",
+  PAS: "00",
+  CCST: "00",
+  CCSM: "00",
+  batch: "BATCH-12",
+  ranking: "0"
+}
+
+export default lcnData
