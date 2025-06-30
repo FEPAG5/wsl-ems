eff88d853bec6ff17f7e37894d7adbe4638aea2c
Gino Arwin G. Santos <ggsantos_0415@proton.me>
2025-06-30 22:24:37 +0800

    Create A02-1234501.ts

---
+++ src/data/LCN/BATCH-02/A02-1234501.ts
@@ -0,0 +1,17 @@
+const lcnData = {
+  lcn: "A02-1234501",
+  name: "Legal Name",
+  issued: "Issued Date",
+  expiration: "Expiry Date",
+  registration: "Latest Registration Date",
+  FWE: "00",
+  SJE: "00",
+  EP: "00",
+  PAS: "00",
+  CCST: "00",
+  CCSM: "00",
+  batch: "BATCH-02",
+  ranking: "0"
+}
+
+export default lcnData
