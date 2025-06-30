d6b8d78aecc8a4615003eda745deec41f9e3634b
Gino Arwin G. Santos <ggsantos_0415@proton.me>
2025-06-30 22:28:07 +0800

    Create A17-1234501.ts

---
+++ src/data/LCN/BATCH-17/A17-1234501.ts
@@ -0,0 +1,17 @@
+const lcnData = {
+  lcn: "A17-1234501",
+  name: "TEST",
+  issued: "Aug 03,2024",
+  expiration: "Aug 03, 2026",
+  registration: "Aug 03, 2024",
+  FWE: "9",
+  SJE: "14.2",
+  EP: "9.8",
+  PAS: "15",
+  CCST: "19",
+  CCSM: "23",
+  batch: "BATCH-09",
+  ranking: "2"
+}
+
+export default lcnData
