0e9581d2e5e833def9084124a6fcef188f42384f
FEPAG5 <fepag_05@yahoo.com>
2025-06-30 19:54:31 +0800

    Update A09-240804.ts

--- src/data/LCN/BATCH-09/A09-240804.ts
+++ src/data/LCN/BATCH-09/A09-240804.ts
@@ -3,7 +3,7 @@ const lcnData = {
   name: "John Carlo Carina",
   issued: "Aug 03, 2024",
   expiration: "Aug 03, 2026",
-  registration: "",
+  registration: "Aug 03, 2024",
   FWE: "8.2",
   SJE: "11",
   EP: "5",
