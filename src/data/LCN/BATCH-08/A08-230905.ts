b0a3deb3d90dff0f7b9d533fd78ee723097c3757
FEPAG5 <fepag_05@yahoo.com>
2025-06-30 19:57:59 +0800

    Update A08-230905.ts

--- src/data/LCN/BATCH-08/A08-230905.ts
+++ src/data/LCN/BATCH-08/A08-230905.ts
@@ -3,7 +3,7 @@ const lcnData = {
   name: "Roderick A. Bugnos",
   issued: "Sept 03, 2023",
   expiration: "Sept 03, 2026",
-  registration: "Latest Registration Date",
+  registration: "Sept 03, 2023",
   FWE: "4",
   SJE: "11.5",
   EP: "6",
