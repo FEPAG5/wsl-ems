dbb97acaf7c0d76a29b6c9af8c74dae0d4d020d9
FEPAG5 <fepag_05@yahoo.com>
2025-06-30 19:00:38 +0800

    Create A09-240809.ts

---
+++ src/data/LCN/BATCH-09/A09-240809.ts
@@ -0,0 +1,17 @@
+const lcnData = {
+  lcn: "A09-240809",
+  name: "Karl Manuel M. Tomagos",
+  issued: "Aug 03,2024",
+  expiration: "Augn 03, 2026",
+  registration: "Latest Registration Date",
+  FWE: "9",
+  SJE: "14.2",
+  EP: "9.8",
+  PAS: "15",
+  CCST: "19",
+  CCSM: "23",
+  batch: "BATCH-09",
+  ranking: "0"
+}
+
+export default lcnData
