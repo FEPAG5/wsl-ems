9a3de262aa0569ba464dfed7f78fecc400808728
FEPAG5 <fepag_05@yahoo.com>
2025-07-06 06:22:09 +0800

    Update A05-210806.ts

--- src/data/LCN/BATCH-05/A05-210806.ts
+++ src/data/LCN/BATCH-05/A05-210806.ts
@@ -2,7 +2,7 @@ const lcnData = {
   lcn: "A05-210806",
   name: "Leah S. Manalo",
   issued: "August 03, 2021",
-  expiration: "August 03, 2026",
+  expiration: "August 03, 2023",
   registration: "August 03, 2021",
   FWE: "00",
   SJE: "00",
