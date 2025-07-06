66cce3347d5f23bd22ec37ce4a5acd3666cb0793
Gino Arwin G. Santos <ggsantos_0415@proton.me>
2025-07-06 08:55:36 +0800

    Update LCN_Data.ts

--- src/data/LCN/BATCH-08/LCN_Data.ts
+++ src/data/LCN/BATCH-08/LCN_Data.ts
@@ -1,17 +1,11 @@
-const lcnData = {
-  lcn: "TEMPLATE-13",
-  name: "Legal Name",
-  issued: "Issued Date",
-  expiration: "Expiry Date",
-  registration: "Latest Registration Date",
-  FWE: "00",
-  SJE: "00",
-  EP: "00",
-  PAS: "00",
-  CCST: "00",
-  CCSM: "00",
-  batch: "BATCH-13",
-  ranking: "0"
-}
+// data/LCN/BATCH-02/LCN_Data.ts
+const validLCNs = [
+  "A08-230901",
+  "A08-230902",
+  "A08-230903",
+  "A08-230904",
+  "A08-230905",
+  "A08-230906",
+];
 
-export default lcnData
+export default validLCNs;
