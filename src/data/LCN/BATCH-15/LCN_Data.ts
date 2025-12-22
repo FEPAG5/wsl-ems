3140ffe003206d883b95f2fac24daa032e20ad03
FEPAG5 <fepag_05@yahoo.com>
2025-12-22 13:04:14 +0800

    Update LCN_Data.ts

--- src/data/LCN/BATCH-15/LCN_Data.ts
+++ src/data/LCN/BATCH-15/LCN_Data.ts
@@ -1,5 +1,12 @@
+// data/LCN/BATCH-02/LCN_Data.ts
 const validLCNs = [
-  "TEMPLAT5-15",
+  "A15251201",
+  "A15251202",
+  "A15251203",
+  "A15251204",
+  "A15251205",
+  "A15251206",
+  
 ];
 
 export default validLCNs;
