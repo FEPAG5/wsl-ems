ec0b6204ca790464980ec0dcb2f82785c07f1d92
FEPAG5 <fepag_05@yahoo.com>
2025-12-22 14:48:12 +0800

    Update A10-241007.ts

--- src/data/LCN/BATCH-10/A10-241007.ts
+++ src/data/LCN/BATCH-10/A10-241007.ts
@@ -1,9 +1,9 @@
 const lcnData = {
   lcn: "A10-241007",
   name: "Jhon Edward S. Fermanes",
-  issued: "Oct 26, 2025",
+  issued: "Oct 26, 2024",
   expiration: "Oct 26, 2026",
-  registration: "Oct 26, 2024",
+  registration: "Oct 26, 2025",
   FWE: "8",
   SJE: "12.8",
   EP: "8.7",
