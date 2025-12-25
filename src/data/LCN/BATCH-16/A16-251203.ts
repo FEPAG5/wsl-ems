cab555eb8120a5d4a1cc35a3f3765490e1b23b7a
FEPAG5 <fepag_05@yahoo.com>
2025-12-25 21:26:47 +0800

    Update and rename A16251203.ts to A16-251203.ts

---
+++ src/data/LCN/BATCH-16/A16-251203.ts
@@ -0,0 +1,17 @@
+const lcnData = {
+  lcn: "TEMPLATE-15",
+  name: "Justine E. Natividad, RN",
+  issued: "December 26, 2025",
+  expiration: "December 26, 2026",
+  registration: "December 26, 2025",
+  FWE: "7",
+  SJE: "11.9",
+  EP: "7.1",
+  PAS: "11",
+  CCST: "21.1",
+  CCSM: "25",
+  batch: "BATCH-15",
+  ranking: "3"
+}
+
+export default lcnData
