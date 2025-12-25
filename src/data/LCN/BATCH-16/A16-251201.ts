80dccdc353f452e000693399850983a6ca951ddf
FEPAG5 <fepag_05@yahoo.com>
2025-12-25 21:23:22 +0800

    Update and rename A16251201.ts to A16-251201.ts

---
+++ src/data/LCN/BATCH-16/A16-251201.ts
@@ -0,0 +1,17 @@
+const lcnData = {
+  lcn: "TEMPLATE-15",
+  name: "Winona Kate A. Tumlos, RN",
+  issued: "December 26, 2025",
+  expiration: "December 26, 2026",
+  registration: "December 26, 2025",
+  FWE: "7.3",
+  SJE: "12",
+  EP: "9.9",
+  PAS: "13.5",
+  CCST: "24.7",
+  CCSM: "25",
+  batch: "BATCH-15",
+  ranking: "1"
+}
+
+export default lcnData
