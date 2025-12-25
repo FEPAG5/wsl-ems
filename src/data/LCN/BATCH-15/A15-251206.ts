90f68c39dad5535b6685885c25e20a3640a65281
FEPAG5 <fepag_05@yahoo.com>
2025-12-25 21:21:40 +0800

    Update and rename A15251206.ts to A15-251206.ts

---
+++ src/data/LCN/BATCH-15/A15-251206.ts
@@ -0,0 +1,17 @@
+const lcnData = {
+  lcn: "TEMPLATE-15",
+  name: "Catherine S. Sembrano",
+  issued: "December 27, 2025",
+  expiration: "December 27, 2026",
+  registration: "December 27, 2025",
+  FWE: "4.8",
+  SJE: "9.7",
+  EP: "4.9",
+  PAS: "8.6",
+  CCST: "22",
+  CCSM: "25",
+  batch: "BATCH-15",
+  ranking: "0"
+}
+
+export default lcnData
