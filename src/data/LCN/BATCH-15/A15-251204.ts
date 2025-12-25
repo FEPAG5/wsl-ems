a77e8bbd78c7ead40384d116f735aa84b70d5a47
FEPAG5 <fepag_05@yahoo.com>
2025-12-25 21:18:15 +0800

    Update and rename A15251204.ts to A15-251204.ts

---
+++ src/data/LCN/BATCH-15/A15-251204.ts
@@ -0,0 +1,17 @@
+const lcnData = {
+  lcn: "TEMPLATE-15",
+  name: "Angelica M. Villanobos, BSN",
+  issued: "December 27, 2025",
+  expiration: "December 27, 2026",
+  registration: "December 27, 2025",
+  FWE: "6.2",
+  SJE: "11",
+  EP: "6.2",
+  PAS: "9.1",
+  CCST: "22.5",
+  CCSM: "21",
+  batch: "BATCH-15",
+  ranking: "0"
+}
+
+export default lcnData
