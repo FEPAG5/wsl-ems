f2a106aa4da9c1a05d85e7fc3ece73691962ea8d
FEPAG5 <fepag_05@yahoo.com>
2025-12-25 21:25:15 +0800

    Update and rename A16251202.ts to A16-251202.ts

---
+++ src/data/LCN/BATCH-16/A16-251202.ts
@@ -0,0 +1,17 @@
+const lcnData = {
+  lcn: "TEMPLATE-15",
+  name: "rachelle Noelle Rafael, RN",
+  issued: "December 26, 2025",
+  expiration: "December 26, 2026",
+  registration: "December 26, 2025",
+  FWE: "7.6",
+  SJE: "12",
+  EP: "9",
+  PAS: "8.9",
+  CCST: "20.7",
+  CCSM: "25",
+  batch: "BATCH-15",
+  ranking: "2"
+}
+
+export default lcnData
