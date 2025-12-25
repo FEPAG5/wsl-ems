dd81e1ed7f853e479bb57534f02bc5b5e8f1b1ff
FEPAG5 <fepag_05@yahoo.com>
2025-12-25 21:45:53 +0800

    Update A15-251204.ts

--- src/data/LCN/BATCH-15/A15-251204.ts
+++ src/data/LCN/BATCH-15/A15-251204.ts
@@ -1,9 +1,9 @@
 const lcnData = {
   lcn: "A15-251204",
   name: "Angelica M. Villanobos, BSN",
-  issued: "December 27, 2025",
-  expiration: "December 27, 2026",
-  registration: "December 27, 2025",
+  issued: "Dec 27, 2025",
+  expiration: "Dec 27, 2026",
+  registration: "Dec 27, 2025",
   FWE: "6.2",
   SJE: "11",
   EP: "6.2",
