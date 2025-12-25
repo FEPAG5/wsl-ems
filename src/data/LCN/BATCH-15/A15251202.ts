5949d300968123ec26b77f83c75316222a86353c
FEPAG5 <fepag_05@yahoo.com>
2025-12-25 21:13:35 +0800

    Update A15251202.ts

--- src/data/LCN/BATCH-15/A15251202.ts
+++ src/data/LCN/BATCH-15/A15251202.ts
@@ -1,17 +1,17 @@
 const lcnData = {
   lcn: "TEMPLATE-15",
-  name: "Legal Name",
-  issued: "Issued Date",
-  expiration: "Expiry Date",
-  registration: "Latest Registration Date",
-  FWE: "00",
-  SJE: "00",
-  EP: "00",
-  PAS: "00",
-  CCST: "00",
-  CCSM: "00",
+  name: "Kurt Agunday",
+  issued: "December 27, 2025",
+  expiration: "Decemeber 27, 2026",
+  registration: "December 27, 2025",
+  FWE: "6",
+  SJE: "11.3",
+  EP: "8",
+  PAS: "13.4",
+  CCST: "22.5",
+  CCSM: "25",
   batch: "BATCH-15",
-  ranking: "0"
+  ranking: "2"
 }
 
 export default lcnData
