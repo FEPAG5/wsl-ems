9f86d5a012de26305e4c665bd71839d3c14932c5
FEPAG5 <fepag_05@yahoo.com>
2025-12-25 21:28:47 +0800

    Update and rename A16251204.ts to A16-251204.ts

---
+++ src/data/LCN/BATCH-16/A16-251204.ts
@@ -0,0 +1,17 @@
+const lcnData = {
+  lcn: "TEMPLATE-15",
+  name: "Kimwill Darrel B. Pergis, RN",
+  issued: "December 26, 2025",
+  expiration: "December 26, 2026",
+  registration: "December 26, 2025",
+  FWE: "6.6",
+  SJE: "11.6",
+  EP: "7",
+  PAS: "9.1",
+  CCST: "21.7",
+  CCSM: "25",
+  batch: "BATCH-15",
+  ranking: "0"
+}
+
+export default lcnData
