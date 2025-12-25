27c4058aed8313d668bc725d3f8a8ea4c7e8eb1b
FEPAG5 <fepag_05@yahoo.com>
2025-12-25 21:15:56 +0800

    Update and rename A15251203.ts to A15-251203.ts

---
+++ src/data/LCN/BATCH-15/A15-251203.ts
@@ -0,0 +1,17 @@
+const lcnData = {
+  lcn: "TEMPLATE-15",
+  name: "<isalea C. Barroga, RN",
+  issued: "December 27, 2025",
+  expiration: "December 27, 2026",
+  registration: "December 27, 2025",
+  FWE: "6",
+  SJE: "11.1",
+  EP: "6.8",
+  PAS: "12.9",
+  CCST: "22.5",
+  CCSM: "25",
+  batch: "BATCH-15",
+  ranking: "3"
+}
+
+export default lcnData
