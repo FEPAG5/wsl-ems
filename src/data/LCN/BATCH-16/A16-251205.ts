706ff70eacd177ce3f9ba2b12e22b9958951265b
FEPAG5 <fepag_05@yahoo.com>
2025-12-25 21:30:26 +0800

    Update and rename A16251205.ts to A16-251205.ts

---
+++ src/data/LCN/BATCH-16/A16-251205.ts
@@ -0,0 +1,17 @@
+const lcnData = {
+  lcn: "TEMPLATE-15",
+  name: "Neil P. Espinola, RN",
+  issued: "December 26, 2025",
+  expiration: "December 26, 2026",
+  registration: "December 26, 2025",
+  FWE: "5.9",
+  SJE: "11.4",
+  EP: "6.9",
+  PAS: "6.9",
+  CCST: "19",
+  CCSM: "25",
+  batch: "BATCH-15",
+  ranking: "0"
+}
+
+export default lcnData
