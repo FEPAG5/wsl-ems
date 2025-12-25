bfdb5d317dee24bb663155b67b2968c7381cc0b5
FEPAG5 <fepag_05@yahoo.com>
2025-12-25 21:32:13 +0800

    Update and rename A16251206.ts to A16-251206.ts

---
+++ src/data/LCN/BATCH-16/A16-251206.ts
@@ -0,0 +1,17 @@
+const lcnData = {
+  lcn: "TEMPLATE-15",
+  name: "Efren Miguell A. Balmeo, RN",
+  issued: "December 26, 2025",
+  expiration: "December 26, 2026",
+  registration: "December 26, 2025",
+  FWE: "5.9",
+  SJE: "11.3",
+  EP: "7.4",
+  PAS: "7.9",
+  CCST: "17.5",
+  CCSM: "25",
+  batch: "BATCH-15",
+  ranking: "0"
+}
+
+export default lcnData
